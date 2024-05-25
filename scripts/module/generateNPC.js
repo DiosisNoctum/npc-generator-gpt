d3c240c903cedef323a5a51b623dd6ed71967745
DiosisNoctum <sonicboom8448@gmail.com>
2024-05-25 01:21:02 -0700

    Update generateNPC.js

--- scripts/module/generateNPC.js
+++ scripts/module/generateNPC.js
@@ -1,4 +1,4 @@
-import { COSTANTS, isRequesting, npcGenGPTLib } from "./lib.js";
+import { CONSTANTS, isRequesting, npcGenGPTLib } from "./lib.js";
 import { npcGenGPTDataStructure } from "./dataStructures.js";
 
 export class npcGenGPTGenerateNPC extends Application {
@@ -9,9 +9,9 @@ export class npcGenGPTGenerateNPC extends Application {
 
     static get defaultOptions() {
         return mergeObject(super.defaultOptions, {
-            id: COSTANTS.MODULE_ID,
+            id: CONSTANTS.MODULE_ID,
             title: game.i18n.localize("npc-generator-gpt.dialog.title"),
-            template: `modules/${COSTANTS.MODULE_ID}/templates/${COSTANTS.TEMPLATE.DIALOG}`,
+            template: `modules/${CONSTANTS.MODULE_ID}/templates/${CONSTANTS.TEMPLATE.DIALOG}`,
             width: 300,
             height: 370
         });
@@ -41,7 +41,7 @@ export class npcGenGPTGenerateNPC extends Application {
                 return `<option value="${subtype.value}">${subtype.label}</option>`;
             }).join('');
         };
-        const label = game.i18n.localize(`npc-generator-gpt.dialog.subtype.${((npcType === 'npc') ? 'class' : 'label')}`);
+        const label = game.i18n.localize(`npc-generator-gpt.dialog.subtype.${(npcType === 'npc') ? 'class' : 'label'}`);
         this.element.find("label[for='subtype']").text(`${label}:`);
         this.element.find("#subtype").html(generateOptions(npcType, true));
         this.element.find("#cr").html(generateOptions('cr', npcType === 'npc'));
@@ -49,7 +49,7 @@ export class npcGenGPTGenerateNPC extends Application {
 
     async initGeneration() {
         if (isRequesting) {
-            ui.notifications.warn(`${COSTANTS.LOG_PREFIX} ${game.i18n.localize("npc-generator-gpt.status.wait")}`);
+            ui.notifications.warn(`${CONSTANTS.LOG_PREFIX} ${game.i18n.localize("npc-generator-gpt.status.wait")}`);
             return;
         }
 
@@ -75,7 +75,6 @@ export class npcGenGPTGenerateNPC extends Application {
             this.data.details[category] = npcGenGPTLib.getSelectedOption(dialogCategory);
         });
         const { cr, race, type, subtype } = this.data.details;
-        subtype.value = (type.value === 'commoner') ? type.value : subtype.value;
         this.data.details.optionalName = this.element.find('#name').val();
         this.data.details.sheet = (type.value === 'commoner') ? 'npc-generator-gpt.dialog.subtype.label' : 'npc-generator-gpt.dialog.subtype.class';
         this.data.abilities = this.generateNpcAbilities(subtype.value, cr.value);
@@ -86,17 +85,25 @@ export class npcGenGPTGenerateNPC extends Application {
     }
 
     initQuery() {
-        const { optionalName, gender, race, subtype, alignment } = this.data.details;
+        const { optionalName, gender, race, subtype, alignment, optionalContext, cr } = this.data.details;
         let options = `${gender.label}, ${race.label}, ${subtype.label}, ${alignment.label}`;
         if (optionalName) options = `(${game.i18n.localize("npc-generator-gpt.query.name")}: ${optionalName}) ${options}`; 
-        return npcGenGPTDataStructure.getGenerateQueryTemplate(options)
+        if (optionalContext) options = `(${game.i18n.localize("npc-generator-gpt.query.context")}: ${optionalContext}) ${options}`; 
+        return npcGenGPTDataStructure.getGenerateQueryTemplate(options, !!optionalContext) + `, "uniqueMagicalWeapon": { "cr": ${cr} }`;
     }
 
     mergeGptData(gptData) {
-        const { name: gptName, spells, items, appearance, background, roleplaying, readaloud } = gptData;
+        const { name: gptName, spells, items, appearance, background, roleplaying, readaloud, uniqueMagicalWeapon } = gptData;
         this.data.name = gptName;
         this.data.spells = spells;
         this.data.items = items;
+        this.data.uniqueMagicalWeapon = uniqueMagicalWeapon; // Store unique magical weapon details
+        if (gptData.strength) this.data.abilities.str.value = gptData.strength;
+        if (gptData.dexterity) this.data.abilities.dex.value = gptData.dexterity;
+        if (gptData.constitution) this.data.abilities.con.value = gptData.constitution;
+        if (gptData.wisdom) this.data.abilities.wis.value = gptData.wisdom;
+        if (gptData.intelligence) this.data.abilities.int.value = gptData.intelligence;
+        if (gptData.charisma) this.data.abilities.cha.value = gptData.charisma;
         this.data.details = {
             ...this.data.details,
             source: "NPC Generator (GPT)",
@@ -112,8 +119,8 @@ export class npcGenGPTGenerateNPC extends Application {
     async createNPC() {
         try {
             const { abilities, attributes, details, name, skills, traits, currency } = this.data;
-            const fakeAlign = (game.settings.get(COSTANTS.MODULE_ID, "hideAlignment")) ? game.i18n.localize("npc-generator-gpt.sheet.unknown") : details.alignment.label;
-            const bioContent = await npcGenGPTLib.getTemplateStructure(COSTANTS.TEMPLATE.SHEET, this.data);
+            const fakeAlign = (game.settings.get(CONSTANTS.MODULE_ID, "hideAlignment")) ? game.i18n.localize("npc-generator-gpt.sheet.unknown") : details.alignment.label;
+            const bioContent = await npcGenGPTLib.getTemplateStructure(CONSTANTS.TEMPLATE.SHEET, this.data);
 
             const npc = await Actor.create({ name: name, type: "npc" });
             await npc.update({
@@ -144,60 +151,47 @@ export class npcGenGPTGenerateNPC extends Application {
             npcGenGPTLib.addItemstoNpc(npc, comp.items, this.data.items);
             npcGenGPTLib.addItemstoNpc(npc, comp.spells, this.data.spells);
 
+            // Create unique magical weapon item
+            const magicalWeaponData = await this.createUniqueMagicalWeaponItem(this.data.uniqueMagicalWeapon);
+            if (magicalWeaponData) {
+                await npc.createEmbeddedDocuments("Item", [magicalWeaponData]);
+            }
+
             npc.sheet.render(true);
 
             this.close();
-            ui.notifications.info(`${COSTANTS.LOG_PREFIX} ${game.i18n.format("npc-generator-gpt.status.done", { npcName: name })}`);
+            ui.notifications.info(`${CONSTANTS.LOG_PREFIX} ${game.i18n.format("npc-generator-gpt.status.done", { npcName: name })}`);
         } catch (error) {
-            console.error(`${COSTANTS.LOG_PREFIX} Error during NPC creation:`, error);
-            ui.notifications.error(`${COSTANTS.LOG_PREFIX} ${game.i18n.localize("npc-generator-gpt.status.error3")}`);
+            console.error(`${CONSTANTS.LOG_PREFIX} Error during NPC creation:`, error);
+            ui.notifications.error(`${CONSTANTS.LOG_PREFIX} ${game.i18n.localize("npc-generator-gpt.status.error3")}`);
         }
     }
 
-    generateNpcAbilities(npcSubtype, npcCR) {
-        const npcStats = npcGenGPTDataStructure.subtypeData[npcSubtype];
-        const profAbilities = (npcSubtype === 'commoner')
-            ? npcGenGPTLib.getRandomFromPool(npcStats.save.pool, npcStats.save.max)
-            : npcStats.save;
-        const npcAbilities = npcGenGPTLib.getNpcAbilities(profAbilities);
-        return npcGenGPTLib.scaleAbilities(npcAbilities, npcCR)
-    }
-
-    generateNpcAttributes(npcRace, npcSubtype, npcCR) {
-        const raceData = npcGenGPTDataStructure.raceData[npcRace];
-        const subtypeData = npcGenGPTDataStructure.subtypeData[npcSubtype];
-        const measureUnits = game.settings.get(COSTANTS.MODULE_ID, "movementUnits") ? 'm' : 'ft';
+    async createUniqueMagicalWeaponItem(uniqueMagicalWeapon) {
         return {
-            hp: npcGenGPTLib.getNpcHp(npcCR, this.data.abilities.con.value, raceData.size),
-            ac: npcGenGPTLib.getNpcAC(npcCR),
-            spellcasting: subtypeData[npcSubtype]?.spellcasting && 'int',
-            movement: { ...((measureUnits === 'm') ? npcGenGPTLib.convertToMeters(raceData.movement) : raceData.movement), units: measureUnits },
-            senses: { ...((measureUnits === 'm') ? npcGenGPTLib.convertToMeters(raceData.senses) : raceData.senses), units: measureUnits }
-        }
+            name: uniqueMagicalWeapon.name,
+            type: "weapon",
+            data: {
+                description: { value: uniqueMagicalWeapon.description },
+                damage: { parts: [[`1d8 + @mod + ${uniqueMagicalWeapon.damageBonus}`, uniqueMagicalWeapon.damageType]] },
+                properties: uniqueMagicalWeapon.properties,
+                rarity: uniqueMagicalWeapon.rarity,
+                magic: true,
+                scaling: uniqueMagicalWeapon.scaling
+            },
+            effects: this.createWeaponEffects(uniqueMagicalWeapon.effects)
+        };
     }
 
-    generateNpcSkills(npcRace, npcSubtype) {
-        const { pool: defaultPool, max } = npcGenGPTDataStructure.subtypeData[npcSubtype].skills;
-        const pool = (npcRace === 'elf' || npcRace === 'drow')
-            ? npcGenGPTLib.getRandomFromPool(defaultPool.filter(skill => skill !== 'prc'), max).concat('prc')
-            : npcGenGPTLib.getRandomFromPool(defaultPool, max);
-
-        return pool.reduce((acc, el) => {
-            acc[el] = { value: 1, ability: npcGenGPTLib.getSkillAbility(el) };
-            return acc;
-        }, {});
-    }
-
-    generateNpcTraits(npcRace, npcSubtype) {
-        const languages = (npcGenGPTDataStructure.raceData[npcRace].lang || []).slice();
-        const subtypeLanguages = (npcGenGPTDataStructure.subtypeData[npcSubtype].lang || []).slice();
-        for (const subLang of subtypeLanguages) if (!languages.includes(subLang)) languages.push(subLang);
-        if (npcRace === 'human' || npcRace === 'halfelf') {
-            languages.push(npcGenGPTLib.getRandomFromPool(npcGenGPTDataStructure.languagesList.filter(lang => !languages.includes(lang)), 1)[0]);
-        }
-        return {
-            languages: languages,
-            size: npcGenGPTDataStructure.raceData[npcRace].size
-        }
+    createWeaponEffects(effects) {
+        return effects.map(effect => ({
+            label: effect.name,
+            icon: effect.icon,
+            origin: null,
+            disabled: false,
+            duration: effect.duration,
+            changes: effect.changes,
+            transfer: true
+        }));
     }
 }
